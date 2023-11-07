9ca58a3c65967792a36beb0d8e916a57087aacce
meinli <86355659+meinli566@users.noreply.github.com>
2023-11-07 16:11:42 +0800

    Update libvirt.go

--- pkg/adaptor/cloud/libvirt/libvirt.go
+++ pkg/adaptor/cloud/libvirt/libvirt.go
@@ -447,7 +447,7 @@ func enableSEV(client *libvirtClient, cfg *domainConfig, vm *vmConfig, domain *l
 	domain.OS.Loader = &libvirtxml.DomainLoader{
 		Path:      vm.firmware,
 		Readonly:  "yes",
-		Stateless: "yes",
+		Secure:    "yes",
 		Type:      "pflash",
 	}
 
